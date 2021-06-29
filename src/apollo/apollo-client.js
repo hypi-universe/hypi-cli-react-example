21fcc98a6af4f876457307c2e0febf5b08ada26b
Eman <eman.cse2008@gmail.com>
2021-06-29 08:17:52 +0200

    change config

--- src/apollo/apollo-client.js
+++ src/apollo/apollo-client.js
@@ -5,13 +5,15 @@ import {
   HttpLink,
 } from "@apollo/client";
 
-const httpLink = new HttpLink({ uri: "https://api.hypi.app/graphql" });
+import config from '../config'
+
+const httpLink = new HttpLink({ uri: config.default_api_domain + "/graphql" });
 
 const authMiddleware = () =>
   new ApolloLink((operation, forward) => {
     // add the authorization to the headers
-    const authToken = 'eyJhbGciOiJSUzI1NiJ9.eyJoeXBpLmxvZ2luIjp0cnVlLCJoeXBpLnVzZXJuYW1lIjoiZW1hbi5jc2UyMDA4QGdtYWlsLmNvbSIsImh5cGkuZW1haWwiOiJlbWFuLmNzZTIwMDhAZ21haWwuY29tIiwiYXVkIjoiMDFGMkdaQkpLSDZSM1RDNkVKRFJFNU5IRzQiLCJpYXQiOjE2MjM5NDQ2MjcsImV4cCI6MTYyNjUzNjYyNywic3ViIjoiMDFGMkdaQkpLQUgxSkNDQlZaUzI0TVQ3VlIiLCJuYmYiOjE2MjM5NDQ2Mjd9.3_4XDk5woGhB-Fmg8Ur2-V0109q1blfCHgQR2eH4mlThnyzLAUh945A1laWVzunj7UesfHFqyfnb-M3nbSJ6ht8J4ly2xvB2eT4VN8_nZhEuFBdiK2RcauRf4fMn4L4J_JSAwQtge3wAZ3zXkSAU4r0dBVZ3F5cOrG_e5CDDwxfA0zNWnCXKZy5WNXFEeVMzLKovu3MfWdXLRKlhAonXUz4d5kOZfmxhTFNzf1TVCBgM1m_u7tatB_lEq31zHM8-ajYF9v8UkzpYG-WvyiOhBMv-j8Vh0Wdf0XmuOnc7A4thQ1Mu_HOoERSh6X8qEYYoGoh6_ADbBSEbWDzwQgs2Ag'
-    const domain = 'supported.apps.hypi.app'
+    const authToken = config.token
+    const domain = config.domain
 
     operation.setContext({
       headers: {
