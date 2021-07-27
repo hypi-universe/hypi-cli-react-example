fd321074744c6b0c345e7755d364f9985a2125f6
Eman <eman.cse2008@gmail.com>
2021-07-27 11:26:55 +0200

    env in netlify.toml

--- src/config.ts
+++ src/config.ts
@@ -1,6 +1,6 @@
 const data = {
   default_api_domain: 'https://api.hypi.app', 
-  domain: process.env.DOMAIN,
-  token: process.env.TOEKN
+  domain: process.env.HYPI_DOMAIN,
+  token: process.env.HYPI_TOEKN
 };
 export default data;
