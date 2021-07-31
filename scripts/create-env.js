0281992d64b199814f21b7011d59ca426d4336c0
Eman <eman.cse2008@gmail.com>
2021-07-31 09:30:53 +0200

    try prepend to config file

--- scripts/create-env.js
+++ scripts/create-env.js
@@ -1,2 +1,13 @@
 const fs = require('fs')
-fs.writeFileSync('./.env', `HYPI_DOMAIN=${process.env.HYPI_DOMAIN}\n`)
+// fs.writeFileSync('./.env', `HYPI_DOMAIN=${process.env.HYPI_DOMAIN}\n`)
+//new
+var data = fs.readFileSync('./src/config.ts'); //read existing contents into data
+var fd = fs.openSync('./src/config.ts', 'w+');
+var buffer = Buffer.alloc() new Buffer(`const HYPI_DOMAIN='${process.env.HYPI_DOMAIN}'\n`);
+
+fs.writeSync(fd, buffer, 0, buffer.length, 0); //write new data
+fs.writeSync(fd, data, 0, data.length, buffer.length); //append old data
+// or fs.appendFile(fd, data);
+fs.close(fd);
+
+// fs.('./src/config.ts', `const HYPI_DOMAIN='${process.env.HYPI_DOMAIN}'\n`)
