92c5198deb30aa81814f2bd867ed37af1324c631
Eman <eman.cse2008@gmail.com>
2021-08-02 13:44:14 +0200

    config.ts

--- src/config.ts
+++ src/config.ts
@@ -1,8 +1,6 @@
-const HYPI_DOMAIN='rapacity.apps.hypi.app'
 const data = {
   default_api_domain: 'https://api.hypi.app', 
-  // domain: process.env.HYPI_DOMAIN,
-  domain: HYPI_DOMAIN,
+  domain: process.env.REACT_APP_HYPI_DOMAIN,
   token: process.env.REACT_APP_HYPI_TOEKN
 };
 export default data;
