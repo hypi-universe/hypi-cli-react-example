099cc45f1f46bce8308c94625a2507117ce67a79
Eman <eman.cse2008@gmail.com>
2021-08-05 13:11:09 +0200

    separate commands

--- plugins/netlify-plugin-hypi/index.js
+++ plugins/netlify-plugin-hypi/index.js
@@ -92,7 +92,9 @@ async function run({
     }
     fs.writeFileSync('netlify.toml', TOML.stringify(data).replace("[\"context.production.environment\"]", "[context.production.environment]"));
 
-    await run.command('git add netlify.toml && git commit -m "[skip ci] Set Hypi domain" && git push origin $BRANCH')
+    await run.command('git add netlify.toml')
+    await run.command('git commit -m "[skip ci] Set Hypi domain"')
+    await run.command('git push origin $BRANCH')
 
   } catch (e) {
     console.error("netlify.toml Parsing error on line " + e.line + ", column " + e.column +
